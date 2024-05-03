bf27d8f33d3a4dad1c3e415c8059dbefb9940125
Tiago Carvalho <136613404+tiago-bozo@users.noreply.github.com>
2024-05-03 11:03:48 -0300

    gerador-senha

---
+++ src/pages/home/index.js
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+import {View, Text, StyleSheet,Image, TouchableOpacity, Modal} from 'react-native'
+import Slider from '@react-native-community/slider'
+import { ModalPassword} from '../../assets/components/modal'
+
+let charset = "abcdefghijlmnopqrstuvwyxzABCDEFGHIJLMNOPQRSTUVWXYZ0123456789"
+
+export function Home (){
+  const [size, setSize] = useState(10)
+  const [passwordValue, setPasswordValue] = useState ("")
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function generatePassword(){
+    
+    let password = "";
+
+    for(let i = 0, n = charset.length; i < size; i++){
+     password += charset.charAt(Math.floor(Math.random() * n))
+    }
+    
+    setPasswordValue(password)
+    setModalVisible(true);
+  }
+  
+   
+
+  return(
+    <View style={styles.container}>
+      <Image
+      source={require("../../assets/logo.png")}
+      style={styles.logo}
+      />
+
+      <Text style={styles.title}>{size} caracteres</Text>
+      
+      <View style={styles.area}>
+        <Slider
+        style={{heigth:50}}
+        minimumValue={6}
+        maximumValue={20}
+        maximumTrackTintColor="#ff0000"
+        minimumTrackTintColor="#000"
+        thumbTintColor="#392de9"
+        value={size}
+        onValueChange={(value) => setSize(value.toFixed (0))}
+        />
+
+      </View>
+      <TouchableOpacity style={styles.button} onPress={generatePassword}> 
+        <Text style={styles.buttonText}>Gerar Senha</Text>
+      </TouchableOpacity>
+
+      <Modal visible={modalVisible} animationType="fade" transparent={true}>
+        <ModalPassword password={passwordValue} handleClose={ ()=> setModalVisible(false)}/>
+      </Modal>
+
+    </View>
+  )
+}
+
+const styles= StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: "#f3f3ff",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo:{
+    marginBottom:60
+  },
+  area:{
+    marginTop:14,
+    marginBottom:14,
+    width:"80%",
+    backgroundColor:"#fff",
+    borderRadius:8,
+    padding:6, 
+    
+  },
+  button:{
+    backgroundColor:"#392de9",
+    width:"80%",
+    height:50,
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius:30,
+    marginTop:10,
+  },
+  buttonText:{
+    color:"#fff",
+    fontSize:15,
+    
+  }, 
+  title:{
+    fontSize:25,
+    fontWeight:'bold',
+  }
+})
---
+++ src/routes.js
@@ -0,0 +1,46 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Home } from "./pages/home";
+import {Passwords} from './pages/passwords'
+
+import {Ionicons} from '@expo/vector-icons'
+
+const Tab = createBottomTabNavigator();
+
+export function Routes(){
+    return(
+        <Tab.Navigator>
+            <Tab.Screen
+            name="home"
+            component={Home}
+            options={{
+                tabBarShowLabel:false,
+                headerShown: false,
+                tabBarIcon: ({focused, size, color}) => {
+                    if(focused){
+                        return <Ionicons sinze={size} color={color} name="home"/>
+                    }
+                    return <Ionicons sinze={size} color={color} name="home-outline"/>
+                }
+            }}
+            />
+
+        <Tab.Screen
+            name="passwords"
+            component={Passwords}
+            options={{
+                tabBarShowLabel:false,
+                headerShown: false,
+                tabBarIcon: ({focused, size, color}) => {
+                    if(focused){
+                        return <Ionicons sinze={size} color={color} name="lock-closed"/>
+                    }
+                    return <Ionicons sinze={size} color={color} name="lock-closed-outline"/>
+                }
+            }}
+            />
+        </Tab.Navigator>
+    )
+}
+
+
+            
