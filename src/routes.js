938c4ea497548ccad895bbe8478acd6d92728b5c
Tiago Carvalho <136613404+tiago-bozo@users.noreply.github.com>
2024-05-03 20:35:13 -0300

    error

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { Home } from "./pages/home";
 import {Passwords} from './pages/passwords'
 
+
 import {Ionicons} from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator();
